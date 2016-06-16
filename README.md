bc2360ab58e4e7eee53e95a53d050cfe9db9c57a
demun <hjm01@naver.com>
2016-06-16 20:00:00 +0900

    first upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # jquery-smooth-move
+
+클릭하면 해당 타겟으로 이동하는 스크립트입니다.
+
+별도의 설정없이 `< a href="#target"></a>` 만 지정하면 이동합니다.
+
+jQuery 가 있어야 합니다.
+
+## Demo
+
+[http://htmlpreview.github.io/?https://github.com/demun/jquery-smooth-move/demo.html](demo.html)
+
+
+## Install
+
+```sh
+bower install jquery-smooth-move
+```
+
+## Usage
+
+```html
+<a href="#target">target</a>
+
+<br><br><br><br><br><br><br><br><br>
+
+<h1 id="target">Target</h1>
+```
---
+++ jquery-smooth-move.js
@@ -0,0 +1,23 @@
+/**
+ * jquery-smooth-move.js
+ *
+ * Licensed under MIT (http://opensource.org/licenses/MIT)
+ */
+
+$(document).ready(function() {
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').on('click', function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 600);
+                    return false;
+                }
+            }
+        });
+    });
+});
+
